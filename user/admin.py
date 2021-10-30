3194395e99668c39c44dce9727504e0bb4f152bb
Naima Osman <naimaosman203@gmail.com>
2021-10-30 20:01:14 +0100

    message model

--- user/admin.py
+++ user/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from user.models import ServiceUser, Provider, TextMessage
+from user.models import ServiceUser, Provider
 
 admin.site.register(Provider)
 admin.site.register(ServiceUser)
-admin.site.register(TextMessage)
