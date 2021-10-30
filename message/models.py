f4b1f72985296c8417d59046ba3ba42991599fd7
Abdulrahman Jama <apjama@Abdulrahmans-MacBook-Pro-2.local>
2021-10-30 17:45:29 +0100

    added sms capabilities

---
+++ message/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+
+# TODO: write a database model that holds all the message that went sent to a user.
+
+# what the message said. who the message was sent to and what time the message was sent.
